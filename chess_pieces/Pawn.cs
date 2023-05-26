3c68e9d1e0efca0ea2e2aa28c34e3823e7159592
Jimmy <jzassick@gmail.com>
2023-05-26 12:57:23 -0400

    Added pawn class

---
+++ chess_pieces/Pawn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChess.chess_pieces
+{
+    internal class Pawn
+    {
+        public Pawn()
+        {
+        }
+    }
+}
