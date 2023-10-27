3618f94a0e4d4c207df96ec09c8e5af365abad21
Jimmy <jzassick@gmail.com>
2023-10-26 23:08:54 -0400

    implemented pawn move validation logic

--- chess_pieces/Pawn.cs
+++ chess_pieces/Pawn.cs
@@ -21,7 +21,49 @@ namespace ConsoleChess.chess_pieces
 
         public bool isValidMove(int originRow, int originCol, int destRow, int destCol, Tile[,] board)
         {
-            return true;
+            if (this.isBlack)
+            {
+                // check moving forward one move
+                if (originRow == destRow && destCol == originCol+1 && board[destRow, destCol].getPiece() is Empty)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+                // check moving forward two moves on first pawn move
+                else if (originRow == destRow && destCol == originCol+2 && board[destRow, destCol].getPiece() is Empty && board[destRow, destCol-1].getPiece() is Empty && this.notMoved)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+                // check capture move
+                else if ((destRow == originRow+1 || destRow == originRow-1) && destCol == originCol+1 && board[destRow, destCol].getPiece() is not Empty)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+            }
+            else if (!this.isBlack)
+            {
+                // check moving forward one move
+                if (originRow == destRow && destCol == originCol-1 && board[destRow, destCol].getPiece() is Empty)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+                // check moving forward two moves on first pawn move
+                else if (originRow == destRow && destCol == originCol-2 && board[destRow, destCol].getPiece() is Empty && board[destRow, destCol+1].getPiece() is Empty && this.notMoved)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+                // check capture move
+                else if ((destRow == originRow + 1 || destRow == originRow - 1) && destCol == originCol-1 && board[destRow, destCol].getPiece() is not Empty)
+                {
+                    this.notMoved = false;
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
