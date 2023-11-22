889f860e9e4489ee5b289b3281d7bff69719c211
Jimmy <jzassick@gmail.com>
2023-11-22 12:49:25 -0500

    rook move validation logic

--- chess_pieces/Rook.cs
+++ chess_pieces/Rook.cs
@@ -19,7 +19,43 @@ namespace ConsoleChess.chess_pieces
 
         public bool isValidMove(int originRow, int originCol, int destRow, int destCol, Tile[,] board)
         {
-            return true;
+            // move left
+            if (destCol < originCol && destRow == originRow)
+            {
+                for (int i = originCol-1; i > destCol; i--)
+                {
+                    if (board[destRow, i].getPiece() is not Empty) { return false; }
+                }
+                return true;
+            }
+            // move right
+            else if (destCol > originCol && destRow == originRow)
+            {
+                for (int i = originCol+1; i < destCol; i++)
+                {
+                    if (board[destRow, i].getPiece() is not Empty) { return false; }
+                }
+                return true;
+            }
+            // move up
+            else if (destCol == originCol && destRow < originRow)
+            {
+                for (int i = originRow - 1; i > destRow; i--)
+                {
+                    if (board[i, destCol].getPiece() is not Empty) { return false; }
+                }
+                return true;
+            }
+            // move down
+            else if (destCol == originCol && destRow > originRow)
+            {
+                for (int i = originRow + 1; i < destRow; i++)
+                {
+                    if (board[i, destCol].getPiece() is not Empty) { return false; }
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
